aea6eae443053c90d0e8947beca2b16f95d991f9
Peter Andrusyszyn <andrusp@oneonta.edu>
2017-05-06 12:21:39 -0400

    formatting changes

--- duffers/views.py
+++ duffers/views.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Golfer, Course, Score, Champs, HoleInOne
 from django.template import loader
-from django.db.models import Avg, Count
 
 def index(request):
     template = loader.get_template('duffers/index.html')
     return HttpResponse(template.render())
+
 def golfers(request):
     golfer_list =Golfer.objects.order_by('last_name','first_name')
     template = loader.get_template('duffers/golfers.html')
@@ -28,21 +26,17 @@ def courses(request):
 
 def scores_by_golfer(request,my_golfer):
     score_list = Score.objects.filter(golfer_id=my_golfer).order_by('course','golfer','play_date')
-    avg_score = Score.objects.filter(golfer_id=my_golfer).aggregate(Avg('par'))
     template = loader.get_template('duffers/scores_by_golfer.html')
     context = {
         'score_list': score_list,
-        'avg_score':avg_score,
     }
     return HttpResponse(template.render(context, request))
 
 def scores_by_course(request,my_course):
     scores_list = Score.objects.filter(course_id=my_course).order_by('course','golfer','play_date')
-    avg_score = Score.objects.filter(course_id=my_course).aggregate(Avg('par'))
     template = loader.get_template('duffers/scores_by_course.html')
     context = {
         'scores_list': scores_list,
-        'avg_score':avg_score,
     }
     return HttpResponse(template.render(context, request))
     
@@ -56,26 +50,44 @@ def champs(request):
     return HttpResponse(template.render(context, request))    
     
     
-def hcap(request):
+def handicap(request):
     golfer_list =Golfer.objects.order_by('last_name','first_name')
     template = loader.get_template('duffers/golfers.html')
     for golfers in golfer_list:
-      scores_list = Score.objects.filter(golfer_id=golfers.id,hcap="Y")
-      differential = 0
-      score_count = 0
-      for scores in scores_list:
-        score_count = score_count+1
-        differential= differential + scores.par - scores.course.par
- #    score_count = score_count+1
-      if score_count>0:
-        myhcap=differential/score_count*.96
-      else: myhcap=0 
-      if myhcap>36:myhcap=36
-      if score_count==0: myhcap=0 
-      Golfer.objects.filter(id=golfers.id).update(handicap=myhcap) 
+        scores_list = Score.objects.filter(golfer_id=golfers.id,hcap="Y")
+        differential = 0
+        score_count = 0
+        for scores in scores_list:
+            score_count = score_count+1
+            differential= differential + scores.par - scores.course.par
+        if score_count>0:
+            myhcap=differential/score_count*.96
+        else: myhcap=0 
+        if myhcap>36:myhcap=36
+        if score_count==0: myhcap=0 
+        Golfer.objects.filter(id=golfers.id).update(handicap=myhcap) 
     context = {
         'golfer_list': golfer_list,
     }          
     return HttpResponse(template.render(context, request)) 
+
+def avg_to_par(request):
+    golfer_list=Golfer.objects.order_by(id)
+    for golfers in golfer_list:
+        scores_list = Score.objects.filter(golfer_id=golfers.id)
+        for scores in scores_list:
+            par3s = 0
+            par4s = 0
+            par5s = 0
+            par3  = 0
+            par4 = 0
+            par5 = 0
+            if scores_list.course.par1==3:
+                par3s = par3s +1
+                par3 = par3 + scores.par1
+    return (par3)            
+        
+        
+    
      
      
