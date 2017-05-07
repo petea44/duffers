24e5c1c6ed71ed7a5d109103facdac23bdd5ae2e
Peter Andrusyszyn <andrusp@oneonta.edu>
2017-05-07 10:27:59 -0400

    add course averages for golfers. Made things even prettier

--- duffers/templatetags/golf_tags.py
+++ duffers/templatetags/golf_tags.py
@@ -1,5 +1,6 @@
 from django import template
-
+from duffers.models import Golfer, Score
+from django.db.models import Avg, Count
 register = template.Library()
 
 @register.simple_tag
@@ -10,10 +11,31 @@ def cutit(value, arg):
 @register.simple_tag
 def score_color(ppar, cpar):
     """ set color for scores based on birdy, par, bogey """
+    if ppar == 1:
+        return "w3-yellow w3-round-xxlarge"
+    if ppar == cpar-2:
+       return "w3-orange w3-round-xxlarge"
     if ppar == cpar: 
-        return "w3-green"
+        return "w3-green w3-round-large"
     if ppar<cpar:
-        return "w3-red"
+        return "w3-red w3-round-large"
     if ppar == (cpar+1):
-        return "w3-blue"
-    return "black"
+        return "w3-blue w3-round-large"
+    return ""
+
+@register.simple_tag
+def my_golfer(golfer_id):
+    golfguy = Golfer.objects.filter(id=golfer_id)
+    for myguy in golfguy:
+        thename = myguy.last_name
+    return thename
+
+@register.simple_tag
+def crse_avg(my_golfer, my_course):
+    myquery = Score.objects.filter(golfer_id=my_golfer, course_id=my_course).aggregate(avg=Avg('par'))
+    #for key, value in myquery:
+      # my_avg = value
+    return  int(round(myquery['avg']))
+        
+    
+     
